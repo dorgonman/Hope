29671c6e869da386aff51b653869dd4fce984578
dorgon <dorgonman@hotmail.com>
2015-06-14 21:29:08 +0800

    add part1

---
+++ Source/Hope/HopeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HopeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOPE_API AHopeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
